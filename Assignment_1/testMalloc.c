48334c8a3d86f7149917ea83ff7d7b5412e0dbbc
np204 <nprocks956@gmail.com>
2024-10-09 14:46:02 -0400

    Update testMalloc.c

--- Assignment_1/testMalloc.c
+++ Assignment_1/testMalloc.c
@@ -19,6 +19,6 @@ int main(int argc, char **argv)
 {
     printf("Size of struct LL node %lu\n", sizeof(struct oLL));
     struct oLL* node = malloc(sizeof(struct oLL));
-    printf("Size of Struct LL node payload returned from malloc %lu\nIntentional leak to show that the 8 byte header is added and that the 20 byte struct still malloc's 24 bytes (no header) \n", sizeof(*node));
+    printf("Leak below shows that the 20 byte struct is aligned to 8 bytes (24 bytes) plus the 8 byte header, leading to 32 byte leak.\n");
     
 }
